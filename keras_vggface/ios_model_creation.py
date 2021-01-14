df59f4804f208aed3690838fbacbefe2955f03c1
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2021-01-14 21:45:24 +0000

    Add CoreML model example separate preprocessing with TensorFlow (currently giving segmentation fault)

--- keras_vggface/ios_model_creation.py
+++ keras_vggface/ios_model_creation.py
@@ -2,6 +2,9 @@ import coremltools as ct
 
 from keras_vggface import VGGFace
 from keras_vggface.strings_model_metadata import VggFaceMetadata
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from keras_vggface import utils
 
 COREML_FILE_FORMAT = ".mlmodel"
 
@@ -34,8 +37,6 @@ def write_metadata(model):
 
 def core_ml_example():
     """Example usage to get face embeddings from cropped image of human face"""
-    from tensorflow.keras.preprocessing import image
-
     coreml_model = ct.models.MLModel("Face.mlmodel")
     img = image.load_img('../image/ajb.jpg', target_size=(224, 224))
     output_dictionary = coreml_model.predict({"input_1": img})
@@ -43,13 +44,30 @@ def core_ml_example():
     print("CoreML embeddings: ", embeddings)
 
 
+def create_core_ml_for_tensorflow_preprocessing():
+    input = ct.TensorType(shape=(1, 224, 224, 3))
+    keras_model = VGGFace(model="senet50", pooling="avg", include_top=False, input_shape=(224, 224, 3))
+    coreml_model = ct.convert(keras_model, inputs=[input])
+    write_metadata(coreml_model)
+    coreml_model.save("Face-without-preprocessing.mlmodel")
+
+def core_ml_with_tensorflow_preprocessing():
+    """
+    Used to isolate why numbers are significantly different from Android and Python output.
+    """
+    img = image.load_img('../image/ajb.jpg', target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = utils.preprocess_input(x, version=2)
+
+    coreml_model = ct.models.MLModel("Face-without-preprocessing.mlmodel")
+    output_dictionary = coreml_model.predict({"input_1": x})
+    embeddings = output_dictionary["Identity"][0]
+    print("CoreML embeddings (tensorflow preprocessing): ", embeddings)
+
+
 def tensorflow_example():
     """This example uses TensorFlow instead of CoreML, and was found to give consistent numbers to CoreML"""
-    # Imports for this example only
-    import numpy as np
-    from tensorflow.keras.preprocessing import image
-    from keras_vggface import utils
-
     model = VGGFace(model="senet50", pooling="avg", include_top=False)
     img = image.load_img('../image/ajb.jpg', target_size=(224, 224))
     x = image.img_to_array(img)
@@ -58,12 +76,17 @@ def tensorflow_example():
     embeddings = model.predict(x)
     print("TensorFlow embeddings: ", embeddings)
 
+
 if __name__ == "__main__":
     # This code is used in https://github.com/popsa-hq/prototype.face-similarity/tree/benb/mobilisation/mobile
 
     # Example, to test the output
-    core_ml_example()
-    tensorflow_example()
+    # core_ml_example()
+    # tensorflow_example()
+
+    # Removing Preprocessing from CoreML
+    # create_core_ml_for_tensorflow_preprocessing()
+    core_ml_with_tensorflow_preprocessing()
 
     # First attempt: Model does not do resizing and preprocessing is done
     # face_model = VGGFace(model="senet50", pooling="avg", include_top=False, input_shape=(224, 224, 3))
